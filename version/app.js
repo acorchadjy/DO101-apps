475f304b7fbe0166f0fe8223d9919061815a9e14
Alejandro <acorchado@viewnext.com>
2020-03-18 22:28:01 +0100

    uodate to verison 6

--- version/app.js
+++ version/app.js
@@ -4,7 +4,7 @@ app = express();
 var response;
 
 app.get('/', function (req, res) {
-    response = 'This is version 5 of the app.' + '\n';
+    response = 'This is version 6 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
